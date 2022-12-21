be8ace40a786822c376fbd2510cc7bf910d697b2
Branden Palmer <brandenpalmer08@gmail.com>
2022-12-21 21:32:01 +0700

    fix close fn on modal

--- app/new-meme/[...pageId]/page.tsx
+++ app/new-meme/[...pageId]/page.tsx
@@ -1,20 +1,18 @@
+"use client";
 import Image from "next/image";
 import NotFoundPage from "./NotFound";
 import memeList from "../../../components/memelist.json";
-import EditMemeModal from "../../../components/EditMemeModal/EditMemeModal";
+import withMemeModal, {
+  WithModalPageProps,
+} from "../../../components/EditMemeModal";
 import LoadingHorse from "../../../imgs/loading-horse.gif";
 import {
   isInvalidPage,
   getMemeEndPoints,
 } from "../../../components/PageNav/PageUtils";
-import Link from "next/link";
 
-interface Props {
-  params: { ids: string[] };
-}
-
-export default function DynamicNewMemePage({ params }: Props) {
-  const [pageId, memeId] = params.ids;
+const DynamicNewMemePage = ({ ModalButton, props }: WithModalPageProps) => {
+  const { pageId } = props.params;
 
   if (isInvalidPage(pageId)) return <NotFoundPage />;
 
@@ -24,13 +22,15 @@ export default function DynamicNewMemePage({ params }: Props) {
 
   return (
     <div>
-      {memeId && <EditMemeModal memeId={memeId} />}
       <h1 className="text-center font-bold text-lg m-2">Select Your Meme</h1>
       <div className="w-full h-full grid grid-cols-[repeat(auto-fill,_minmax(10rem,_1fr))] overflow-scroll">
         {memeSubList.map((meme) => {
           const title = meme.replaceAll("-", " ");
           return (
-            <div className="flex justify-between bg-white flex-col m-2 rounded-lg p-2 shadow-md">
+            <div
+              key={meme}
+              className="flex justify-between bg-white flex-col m-2 rounded-lg p-2 shadow-md"
+            >
               <h2 className="text-ellipsis overflow-hidden text-center font-medium underline">
                 {title}
               </h2>
@@ -43,16 +43,18 @@ export default function DynamicNewMemePage({ params }: Props) {
                 placeholder="blur"
                 blurDataURL={LoadingHorse.src}
               />
-              <Link
+              <ModalButton
+                memeId={meme}
                 className="px-3 py-1  text-center bg-blue-600 rounded-full border-2 border-black text-black hover:text-white hover:-translate-y-1 duration-300 shadow-md hover:shadow-slate-500 font-medium"
-                href={`/new-meme/${params.ids}/${meme}`}
               >
                 Select
-              </Link>
+              </ModalButton>
             </div>
           );
         })}
       </div>
     </div>
   );
-}
+};
+
+export default withMemeModal(DynamicNewMemePage);
---
+++ components/EditMemeModal/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./EditMemeModal.module.css";
+
+type ModalButtonProps = {
+  memeId: string;
+} & JSX.IntrinsicElements["button"];
+
+type ModalButtonType = React.FC<ModalButtonProps>;
+
+export interface WithModalPageProps {
+  ModalButton: ModalButtonType;
+  props: any;
+}
+
+export default function withMemeModal(
+  WrappedComponent: React.FC<WithModalPageProps>
+) {
+  return (props: any) => {
+    const [meme, setMeme] = useState<null | string>(null);
+
+    const ModalButton: ModalButtonType = (pProps) => {
+      const { memeId, ...props } = pProps;
+      props.onClick = () => setMeme(memeId);
+      return <button {...props}></button>;
+    };
+
+    const MemeModal = () => {
+      const close = () => setMeme(null);
+      return (
+        <div
+          className="flex absolute top-0 left-0 z-50 w-screen h-screen justify-center items-center transition-opacity  bg-opacity-40 bg-black"
+          onClick={close}
+        >
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            className={"w-1/2 h-1/2 text-black bg-white " + styles.popIn}
+          >
+            {meme}
+          </div>
+        </div>
+      );
+    };
+
+    return (
+      <>
+        {meme && <MemeModal />}
+        <WrappedComponent ModalButton={ModalButton} props={props} />
+      </>
+    );
+  };
+}
