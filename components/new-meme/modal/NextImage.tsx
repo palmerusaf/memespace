61fca6eb873d44bca509292faaf039dbeac6ccc7
Branden Palmer <brandenpalmer08@gmail.com>
2022-12-27 02:24:27 +0700

    add loading gif for modal img

--- components/new-meme/modal/NextImage.tsx
+++ components/new-meme/modal/NextImage.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import LoadingHorse from '@ui/new-meme/loading-horse.gif';
 import ErrorHorse from '@ui/new-meme/error-horse.gif';
@@ -11,22 +11,35 @@ interface Props {
 
 const NextImage = ({ imgSrc, modalId }: Props) => {
   const [img, setImg] = useState(imgSrc);
+  const [loading, setLoading] = useState(true);
   const setErrorImg = () => {
     setImg(ErrorHorse.src);
+    setLoading(false);
   };
+  useEffect(() => {}, [loading]);
   return (
-    <Image
-      src={img}
-      width={300}
-      height={300}
-      onError={setErrorImg}
-      className={
-        'rounded-md shadow-lg shadow-gray-400 aspect-auto w-auto max-h-96 '
-      }
-      alt={modalId.replaceAll('-', ' ')}
-      placeholder='blur'
-      blurDataURL={LoadingHorse.src}
-    />
+    <>
+      <Image
+        src={LoadingHorse.src}
+        alt='Loading Horse'
+        width={300}
+        height={300}
+        className={` rounded-md animate-pulse shadow-lg shadow-gray-400 aspect-auto w-auto max-h-96 ${
+          !loading && 'h-0'
+        }`}
+      />
+      <Image
+        src={img}
+        width={300}
+        height={300}
+        onError={setErrorImg}
+        onLoad={() => setLoading(false)}
+        className={`rounded-md shadow-lg shadow-gray-400 aspect-auto w-auto max-h-96 `}
+        alt={modalId.replaceAll('-', ' ')}
+        placeholder='blur'
+        blurDataURL={LoadingHorse.src}
+      />
+    </>
   );
 };
 
