924b71a0a0cdeb7b9c731b38047278bfbf063713
Branden Palmer <brandenpalmer08@gmail.com>
2023-01-04 18:49:39 +0700

    center label on input comp

--- components/new-meme/modal/input.tsx
+++ components/new-meme/modal/input.tsx
@@ -8,6 +8,12 @@ const Input = (
   { label, value }: Props,
   ref: React.ForwardedRef<HTMLInputElement>
 ) => {
+  const notEmpty =
+    'absolute shadow-lg translate-y-3/4 bg-gray-300 rounded-full scale-75 outline outline-1 outline-white left-1/2 -translate-x-1/2';
+  const empty =
+    'peer-placeholder-shown:translate-y-0 peer-placeholder-shown:translate-x-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:outline-none peer-placeholder-shown:left-11 duration-300 peer-placeholder-shown:shadow-none';
+  const focused =
+    'peer-focus-visible:translate-y-full peer-focus-visible:scale-75 peer-focus-visible:left-1/2 peer-focus-visible:-translate-x-1/2 peer-focus-visible:shadow-lg';
   return (
     <div className='relative w-full h-8 shrink-0 bg-gray-300 border-white border-2 rounded-full shadow-xl'>
       <input
@@ -22,15 +28,7 @@ const Input = (
       <span className='opacity-0 peer-placeholder-shown:opacity-100 peer-focus-within:opacity-0 ml-2 absolute z-10 duration-300'>
         Enter
       </span>
-      <label
-        htmlFor={label}
-        className={
-          'px-2 ' +
-          'absolute shadow-lg translate-y-3/4 bg-gray-300 rounded-full scale-75 outline outline-1  outline-white ' +
-          'peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:outline-none peer-placeholder-shown:left-11 duration-300 peer-placeholder-shown:shadow-none' +
-          ' peer-focus-visible:translate-y-full peer-focus-visible:scale-75 peer-focus-visible:left-0 peer-focus-visible:shadow-lg'
-        }
-      >
+      <label htmlFor={label} className={`px-2 ${notEmpty} ${empty} ${focused}`}>
         {label}
       </label>
     </div>
