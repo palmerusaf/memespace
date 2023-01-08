b7d56dcadab88e468fd7e1ec9c2e5531e4445959
Branden Palmer <brandenpalmer08@gmail.com>
2023-01-08 12:37:28 +0700

    run firebase init w/ emulator, firestore

--- firebase/client-app.ts
+++ firebase/client-app.ts
@@ -1,10 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  connectAuthEmulator,
-  signInWithCredential,
-} from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,7 +18,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
-const firestore = getFirestore(firebase);
-export default firebase;
-
-const auth = getAuth(firebase);
