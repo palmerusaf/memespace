50cb162cbe4673a386559a7d35b01b89a5f2a38d
Branden Palmer <brandenpalmer08@gmail.com>
2023-01-12 15:51:21 +0700

    add login sections

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,17 +20,40 @@ function withAuth(WithAuthComponent: AuthComponent) {
 }
 
 function LoggedOut() {
+  const Divider = ({ label }: { label: string }) => {
+    return (
+      <h2 className='grid grid-cols-[auto,_1fr] w-full items-center'>
+        {/* <span className='h-0 border-b border-black mx-2'></span> */}
+        <span className='font-bold ml-2'>{label}</span>
+        <span className='h-0 border-b border-black mx-2'></span>
+      </h2>
+    );
+  };
   return (
-    <form className='flex flex-col h-full w-full justify-center items-center px-2 gap-2'>
-      <h1 className=''>New Use</h1>
-      <Input label='User Name' />
-      <button className='bg-red-700 py-1 rounded-2xl border-2 w-full border-gray-700 text-white hover:text-white hover:-translate-y-1 duration-300 hover:shadow-gray-900 shadow-md font-bold text-xl mt-4'>
-        Sign in with Google
-      </button>
-      <button className='bg-blue-600 py-1 rounded-2xl border-2 w-full border-gray-700 text-white hover:text-white hover:-translate-y-1 duration-300 hover:shadow-gray-900 shadow-md font-bold text-xl'>
-        Sign in with Facebook
-      </button>
-    </form>
+    <div className='flex flex-col h-full w-full px-2'>
+      <h1 className='w-full text-center font-extrabold text-xl my-4 underline'>
+        Login Below
+      </h1>
+      <form className='flex flex-col w-full justify-center items-center gap-2'>
+        <Divider label='New User' />
+        <Input label='User Name' />
+        <button className='bg-red-700 py-1 rounded-2xl border-2 w-full border-black text-white hover:text-white hover:-translate-y-1 duration-300 hover:shadow-gray-900 shadow-md font-bold text-xl mt-4'>
+          Sign up with Google
+        </button>
+        <button className='bg-blue-600 py-1 rounded-2xl border-2 w-full border-black text-white hover:text-white hover:-translate-y-1 duration-300 hover:shadow-gray-900 shadow-md font-bold text-xl'>
+          Sign up with Facebook
+        </button>
+      </form>
+      <div className='flex flex-col w-full justify-center items-center gap-2'>
+        <Divider label='Returning Users' />
+        <button className='bg-red-700 py-1 rounded-2xl border-2 w-full border-black text-white hover:text-white hover:-translate-y-1 duration-300 hover:shadow-gray-900 shadow-md font-bold text-xl mt-4'>
+          Continue with Google
+        </button>
+        <button className='bg-blue-600 py-1 rounded-2xl border-2 w-full border-black text-white hover:text-white hover:-translate-y-1 duration-300 hover:shadow-gray-900 shadow-md font-bold text-xl'>
+          Continue with Facebook
+        </button>
+      </div>
+    </div>
   );
 }
 
