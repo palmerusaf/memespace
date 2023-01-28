59b34fa8ecda72f45341f7c71fc504472033c0a6
Branden Palmer <brandenpalmer08@gmail.com>
2023-01-28 14:04:43 +0700

    add firebase emulators

--- components/shared/firebase-utils/index.ts
+++ components/shared/firebase-utils/index.ts
@@ -1,6 +1,10 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import {
+  connectAuthEmulator,
+  getAuth,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 
 const firebaseConfig = {
@@ -16,8 +20,10 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
 
 export const db = getFirestore(app);
+connectFirestoreEmulator(db, 'localhost', 9100);
 
 export const useLoggedIn = () => {
   const [loggedIn, setLoggedIn] = useState(false);
