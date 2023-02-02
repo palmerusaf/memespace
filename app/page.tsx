1f2faf01e5442bf5720d3a9c7f8b9b8cd7dd47e6
Branden Palmer <brandenpalmer08@gmail.com>
2023-02-02 12:01:33 +0700

    fix redirects

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
 'use client';
-import { auth } from '@ui/shared/firebase-utils';
+import { auth, useLoggedIn } from '@ui/shared/firebase-utils';
+import { LoadingPage } from '@ui/shared/loading-page';
+import assert from 'assert';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-interface Props {}
-
-const Page = ({}: Props) => {
+const Page = () => {
   const router = useRouter();
+  const { loggedIn } = useLoggedIn();
   useEffect(() => {
-    if (auth.currentUser) {
-      router.replace('/profile');
+    if (loggedIn) {
+      assert(auth.currentUser);
+      router.replace(`/profile/${auth.currentUser.uid}`);
     } else {
       router.replace('/login');
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return <></>;
+  }, [loggedIn]);
+  if (loggedIn)
+    return <LoadingPage loadingMsg='Redirecting to your profile.' />;
+  else return <LoadingPage loadingMsg='Checking login status.' />;
 };
 
 export default Page;
--- app/profile/[uid]/page.tsx
+++ app/profile/[uid]/page.tsx
@@ -1,10 +1,4 @@
 import ComingSoon from '@ui/shared/coming-soon';
-import { db } from '@ui/shared/firebase-utils';
-import { doc, getDoc } from 'firebase/firestore';
-
-async function getUserData(userId: string) {
-  return getDoc(doc(db, 'users', userId));
-}
 
 interface Props {
   params: { uid: string };
