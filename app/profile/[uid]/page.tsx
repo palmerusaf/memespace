85242a2cf709812b26a7f7413fae9da76b2af2e7
Branden Palmer <brandenpalmer08@gmail.com>
2023-02-03 09:58:58 +0700

    pull profile data

--- app/profile/[uid]/page.tsx
+++ app/profile/[uid]/page.tsx
@@ -1,11 +1,22 @@
-import ComingSoon from '@ui/shared/coming-soon';
+'use client';
+import { useProfileQuery } from '@ui/shared/firebase-utils';
+import { LoadingPage } from '@ui/shared/loading-page';
 
 interface Props {
   params: { uid: string };
 }
 
 const Page = ({ params: { uid } }: Props) => {
-  return <ComingSoon page={`Profile ${uid}`} />;
+  const query = useProfileQuery(uid);
+
+  if (query.isLoading) return <LoadingPage loadingMsg='Gathering User Data.' />;
+  if (query.isError) throw new Error('Trouble connecting to database.');
+  if (query.data) {
+    const { userName, profileMeme, createdDate } = query.data;
+    console.log({ userName, profileMeme, createdDate });
+    return <span>data loaded</span>;
+  }
+  if (!query.data) return <span>no data to load</span>;
 };
 
 export default Page;
