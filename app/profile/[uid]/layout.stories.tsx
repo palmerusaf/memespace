24fdfa07408ac5b905254b52b48cd1d7a2b600aa
Branden Palmer <brandenpalmer08@gmail.com>
2023-02-14 13:09:08 +0700

    refactor profile page to layout

---
+++ app/profile/[uid]/layout.stories.tsx
@@ -0,0 +1,107 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
+import { RecievingProfileData } from '@ui/shared/firebase-utils';
+import { Timestamp } from 'firebase/firestore';
+import NextLayout from './layout';
+
+const queryClient = new QueryClient();
+
+export default {
+  title: 'Profile/Layout',
+  component: NextLayout,
+  args: {
+    params: { uid: '', pUseIsOwner: (uid: string) => ({ isOwner: false }) },
+  },
+} as ComponentMeta<typeof NextLayout>;
+
+const Template: ComponentStory<typeof NextLayout> = (args) => (
+  <QueryClientProvider client={queryClient}>
+    <NextLayout {...args} />
+  </QueryClientProvider>
+);
+
+const setQueryWith = (testData: RecievingProfileData | null) => (uid: string) =>
+  useQuery({
+    queryKey: ['test-value'],
+    queryFn: () => testData,
+  });
+
+export const NullData = Template.bind({});
+NullData.args = { pUseProfileQuery: setQueryWith(null) };
+
+export const MemeNotSet = Template.bind({});
+MemeNotSet.args = {
+  pUseProfileQuery: setQueryWith({
+    meme: '',
+    userName: 'foo',
+    createdDate: Timestamp.now(),
+  }),
+};
+
+export const HasOwnership = Template.bind({});
+HasOwnership.args = {
+  pUseIsOwner: (uid) => ({ isOwner: true }),
+  pUseProfileQuery: setQueryWith({
+    meme: '10-Guy',
+    userName: 'foo',
+    createdDate: Timestamp.now(),
+  }),
+};
+
+export const Normal = Template.bind({});
+Normal.args = {
+  pUseProfileQuery: setQueryWith({
+    meme: '10-Guy',
+    userName: 'foo',
+    createdDate: Timestamp.now(),
+  }),
+};
+
+export const UserName20Chars = Template.bind({});
+UserName20Chars.args = {
+  pUseProfileQuery: setQueryWith({
+    meme: '10-Guy',
+    userName: 'aaaaaaaaaaaaaaaaaaaa',
+    createdDate: Timestamp.now(),
+  }),
+};
+
+export const UserNameWithSpaces = Template.bind({});
+UserNameWithSpaces.args = {
+  pUseProfileQuery: setQueryWith({
+    meme: '10-Guy',
+    userName: 'username with space',
+    createdDate: Timestamp.now(),
+  }),
+};
+
+export const OverFlowMeme = Template.bind({});
+OverFlowMeme.args = {
+  pUseProfileQuery: setQueryWith({
+    meme: 'American-Chopper-Argument',
+    userName: 'foo',
+    createdDate: Timestamp.now(),
+  }),
+};
+
+export const NotFoundMeme = Template.bind({});
+NotFoundMeme.args = {
+  pUseProfileQuery: setQueryWith({
+    meme: 'Always-Has-Been',
+    userName: 'foo',
+    createdDate: Timestamp.now(),
+  }),
+};
+
+// export const Error = Template.bind({});
+// Error.args = {
+//   pUseProfileQuery: (uid: string) =>
+//     useQuery({
+//       queryFn: async () => Promise.reject(),
+//     }),
+// };
--- app/profile/[uid]/page.stories.tsx
+++ app/profile/[uid]/page.stories.tsx
@@ -6,17 +6,14 @@ import {
   useQuery,
 } from '@tanstack/react-query';
 import { RecievingProfileData } from '@ui/shared/firebase-utils';
-import { Timestamp } from 'firebase/firestore';
 import Page from './page';
 
 const queryClient = new QueryClient();
 
 export default {
-  title: 'Profile/Page',
+  title: 'Profile/Meme Collection Page',
   component: Page,
-  args: {
-    params: { uid: '', pUseIsOwner: (uid: string) => ({ isOwner: false }) },
-  },
+  args: {},
 } as ComponentMeta<typeof Page>;
 
 const Template: ComponentStory<typeof Page> = (args) => (
@@ -31,77 +28,5 @@ const setQueryWith = (testData: RecievingProfileData | null) => (uid: string) =>
     queryFn: () => testData,
   });
 
-export const NullData = Template.bind({});
-NullData.args = { pUseProfileQuery: setQueryWith(null) };
-
-export const MemeNotSet = Template.bind({});
-MemeNotSet.args = {
-  pUseProfileQuery: setQueryWith({
-    meme: '',
-    userName: 'foo',
-    createdDate: Timestamp.now(),
-  }),
-};
-
-export const HasOwnership = Template.bind({});
-HasOwnership.args = {
-  pUseIsOwner: (uid) => ({ isOwner: true }),
-  pUseProfileQuery: setQueryWith({
-    meme: '10-Guy',
-    userName: 'foo',
-    createdDate: Timestamp.now(),
-  }),
-};
-
-export const Normal = Template.bind({});
-Normal.args = {
-  pUseProfileQuery: setQueryWith({
-    meme: '10-Guy',
-    userName: 'foo',
-    createdDate: Timestamp.now(),
-  }),
-};
-
-export const UserName20Chars = Template.bind({});
-UserName20Chars.args = {
-  pUseProfileQuery: setQueryWith({
-    meme: '10-Guy',
-    userName: 'aaaaaaaaaaaaaaaaaaaa',
-    createdDate: Timestamp.now(),
-  }),
-};
-
-export const UserNameWithSpaces = Template.bind({});
-UserNameWithSpaces.args = {
-  pUseProfileQuery: setQueryWith({
-    meme: '10-Guy',
-    userName: 'username with space',
-    createdDate: Timestamp.now(),
-  }),
-};
-
-export const OverFlowMeme = Template.bind({});
-OverFlowMeme.args = {
-  pUseProfileQuery: setQueryWith({
-    meme: 'American-Chopper-Argument',
-    userName: 'foo',
-    createdDate: Timestamp.now(),
-  }),
-};
-
-export const NotFoundMeme = Template.bind({});
-NotFoundMeme.args = {
-  pUseProfileQuery: setQueryWith({
-    meme: 'Always-Has-Been',
-    userName: 'foo',
-    createdDate: Timestamp.now(),
-  }),
-};
-
-// export const Error = Template.bind({});
-// Error.args = {
-//   pUseProfileQuery: (uid: string) =>
-//     useQuery({
-//       queryFn: async () => Promise.reject(),
-//     }),
-// };
+export const Story = Template.bind({});
+Story.args = {};
