92c94ca043e08c97ace3373ab0575ccac44690fb
Branden Palmer <brandenpalmer08@gmail.com>
2023-02-16 15:57:28 +0700

    add pic to nav bar

--- components/profile/nav-bar.tsx
+++ components/profile/nav-bar.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+import { MadBro, SmallLogo } from '@ui/shared/imgs';
 import { NavLink } from '@ui/shared/nav-link';
 import { useSelectedLayoutSegment } from 'next/navigation';
 
@@ -13,8 +15,20 @@ export function NavBar({
   return (
     <nav className='flex w-full justify-center'>
       <ul className='flex gap-3 rounded-md bg-white px-2 py-0.5 shadow-md md:text-xl'>
-        <NavBarLink href={`/profile/${uid}`}>Meme Collection</NavBarLink>
-        <NavBarLink href={`/profile/${uid}/tracked`}>Tracked Degens</NavBarLink>
+        <NavBarLink
+          href={`/profile/${uid}`}
+          imgSrc={MadBro.src}
+          imgAlt={'Mad Bro'}
+        >
+          MEMES
+        </NavBarLink>
+        <NavBarLink
+          href={`/profile/${uid}/tracked`}
+          imgSrc={SmallLogo.src}
+          imgAlt={'meme people'}
+        >
+          TRACKING
+        </NavBarLink>
       </ul>
     </nav>
   );
@@ -22,13 +36,18 @@ export function NavBar({
   function NavBarLink({
     href,
     children,
+    imgSrc,
+    imgAlt,
   }: {
     href: string;
     children: React.ReactNode;
+    imgSrc: string;
+    imgAlt: string;
   }) {
     let active = href === `/profile/${uid}${segment ? `/${segment}` : ''}`;
     return (
       <NavLink activeClass='font-bold' href={href} active={active}>
+        <img src={imgSrc} alt={imgAlt} />
         {children}
       </NavLink>
     );
