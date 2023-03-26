32996ee984fe91a56cdc35bf1d462e49360ce226
Branden Palmer <brandenpalmer08@gmail.com>
2023-03-27 00:11:49 +0700

    extract mutation button

---
+++ components/shared/mutant-button.tsx
@@ -0,0 +1,49 @@
+import { UseMutationResult } from '@tanstack/react-query';
+import { MouseEventHandler, useEffect, useState } from 'react';
+
+interface Props {
+  mutation: UseMutationResult<unknown, unknown, any, unknown>;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  loadMsg: string;
+  errorMsg: string;
+  successMsg: string;
+  staticMsg: string;
+}
+export const MutantButton = (props: Props) => {
+  const { mutation } = props;
+
+  const [inTimeLimit, setInTimeLimit] = useState(false);
+  useEffect(() => {
+    setInTimeLimit(true);
+    setTimeout(() => {
+      setInTimeLimit(false);
+    }, 1000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mutation.isSuccess]);
+
+  if (mutation.isLoading)
+    return <Button clickable={false}>{props.loadMsg}</Button>;
+  if (mutation.isError) return <Button>{props.errorMsg}</Button>;
+  if (mutation.isSuccess && inTimeLimit)
+    return <Button>{props.successMsg}</Button>;
+  return <Button>{props.staticMsg}</Button>;
+
+  function Button({
+    children,
+    clickable = true,
+  }: {
+    children: React.ReactNode;
+    clickable?: boolean;
+  }) {
+    return (
+      <button
+        onClick={clickable ? props.onClick : undefined}
+        className={`rounded-full bg-blue-500 px-4 text-lg font-semibold text-white shadow-2xl duration-500 hover:-translate-y-1 hover:scale-105 md:text-xl ${
+          mutation.isLoading && 'animate-pulse duration-1000'
+        }`}
+      >
+        {children}
+      </button>
+    );
+  }
+};
