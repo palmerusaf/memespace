e37fa8e8d7f1e9f9267936688ca09bd75076b1e6
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-06 18:45:38 +0700

    repurpose avatar area into user card

--- components/shared/user-card/index.stories.tsx
+++ components/shared/user-card/index.stories.tsx
@@ -1,18 +1,50 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-
 import { UserCard } from './index';
 
 export default {
   title: 'Shared/User Card',
   component: UserCard,
-  args: {},
+  args: {
+    uid: '123',
+  },
 } as ComponentMeta<typeof UserCard>;
 
 const Template: ComponentStory<typeof UserCard> = (args) => (
-  <div className='flex h-screen w-screen items-center justify-center'>
-    <UserCard {...args} />
-  </div>
+  <UserCard {...args}></UserCard>
 );
 
-export const Story = Template.bind({});
-Story.args = {};
+export const MemeNotSet = Template.bind({});
+MemeNotSet.args = {
+  profileMeme: '',
+};
+
+export const Normal = Template.bind({});
+Normal.args = {
+  profileMeme: '10-Guy',
+  userName: 'username',
+};
+
+export const UserName20Chars = Template.bind({});
+UserName20Chars.args = {
+  profileMeme: '10-Guy',
+  userName: 'aaaaaaaaaaaaaaaaaaaa',
+};
+
+export const UserNameWithSpaces = Template.bind({});
+UserNameWithSpaces.args = {
+  profileMeme: '10-Guy',
+  userName: 'username with space',
+};
+
+export const OverFlowMeme = Template.bind({});
+OverFlowMeme.args = {
+  profileMeme: 'American-Chopper-Argument',
+  userName: 'foo',
+};
+
+export const NotFoundMeme = Template.bind({});
+NotFoundMeme.args = {
+  profileMeme: 'Always-Has-Been',
+  userName: 'foo',
+};
--- components/shared/user-card/index.tsx
+++ components/shared/user-card/index.tsx
@@ -1,22 +1,49 @@
-import { ReactNode } from 'react';
-import { getThumbnail, getTitle } from '../api-meme-utils';
-import ImageWithLoadState from '../next-image';
+/* eslint-disable @next/next/no-img-element */
+import { getThumbnail } from '@ui/shared/api-meme-utils';
+import { SmallLogo } from '@ui/shared/imgs';
+import ImageWithLoadState from '@ui/shared/next-image';
+
+export function AvatarPic({ profileMeme }: { profileMeme: string }) {
+  if (profileMeme === '') {
+    return (
+      <div className='aspect-square w-full rounded-full bg-blue-600 shadow-xl'>
+        <img
+          src={SmallLogo.src}
+          alt='Placeholder Meme'
+          className='aspect-square w-full'
+        />
+      </div>
+    );
+  } else {
+    return (
+      <ImageWithLoadState
+        src={getThumbnail(profileMeme)}
+        alt='Placeholder Meme'
+        width={150}
+        height={150}
+        className='aspect-square w-full rounded-full object-cover shadow-xl'
+      />
+    );
+  }
+}
 
 interface Props {
-  buttonArea?: ReactNode;
   uid: string;
   userName: string;
   profileMeme: string;
 }
-export const UserCard = ({ uid, userName, profileMeme, buttonArea }: Props) => {
+
+export function UserCard({ uid, userName, profileMeme }: Props) {
   return (
-    <div className='flex shadow-xl'>
-      <ImageWithLoadState
-        src={getThumbnail(profileMeme)}
-        alt={getTitle({ meme: profileMeme })}
-      />
-      <span className='font-bold underline'>{userName}</span>
-      <span>{buttonArea}</span>
+    <div className='relative flex items-center justify-between gap-3 rounded-md bg-white py-4 px-3 shadow-md md:justify-start md:py-6 md:px-16'>
+      <div className='w-20 md:w-36'>
+        <AvatarPic profileMeme={profileMeme} />
+      </div>
+      <div className='flex flex-col gap-1 text-center md:flex-1 md:gap-9'>
+        <div className='text-xl font-semibold md:text-2xl'>
+          {userName !== '' ? userName : 'No Username'}
+        </div>
+      </div>
     </div>
   );
-};
+}
