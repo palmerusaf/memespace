7d714ea2fbf9c8f9c6b6d665dbb42d86c6cf016b
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-13 11:19:10 +0700

    fix find meme spacing on desktop

--- app/find-memes/layout.tsx
+++ app/find-memes/layout.tsx
@@ -7,7 +7,9 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <div className='-z-10 h-full bg-gray-300'>
-      <div className='h-[calc(100vh-243px)] md:h-full'>{children}</div>
+      <div className='h-[calc(100vh-243px)] md:h-[calc(100vh-125px)]'>
+        {children}
+      </div>
       <div className='fixed left-0 bottom-16 mb-2 flex w-full justify-center md:bottom-2'>
         <PageNav />
       </div>
