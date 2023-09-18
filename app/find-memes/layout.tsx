e8b523380eaab0a54c8ebc31d23c37cd4e058fd6
Branden Palmer <brandenpalmer08@gmail.com>
2023-09-19 00:39:32 +0700

    fix spacing bug on mobile by using new dynamic css units

--- app/find-memes/layout.tsx
+++ app/find-memes/layout.tsx
@@ -6,10 +6,8 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className='-z-10 h-full bg-gray-300'>
-      <div className='h-[calc(100vh-243px)] md:h-[calc(100vh-125px)]'>
-        {children}
-      </div>
+    <div className='-z-10 flex flex-col bg-gray-300'>
+      <div className='grow pb-28 md:pb-32'>{children}</div>
       <div className='fixed left-0 bottom-16 mb-2 flex w-full justify-center md:bottom-2'>
         <PageNav />
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,18 +23,14 @@ const Layout = ({ children }: Props) => {
   return (
     <html>
       <head />
-      <body className='h-screen w-screen overflow-clip'>
+      <body className='flex h-[100dvh] w-screen flex-col overflow-clip'>
         <QueryClientProvider client={queryClient}>
-          <Link href={'/'}>
-            <header className='flex h-14 w-full justify-center bg-blue-600 md:z-50 md:h-16'>
-              <Image src={Logo} width={300} height={56} alt='logo' />
-            </header>
-          </Link>
-          <div className='flex h-[calc(100vh-7.5rem)] flex-col md:grid md:h-[calc(100vh-3.5rem)] md:grid-cols-[160px,auto]'>
+          {Banner()}
+          <div className='flex grow flex-col overflow-scroll md:grid md:h-[calc(100vh-3.5rem)] md:grid-cols-[160px,auto]'>
             <div className='h-full overflow-scroll md:col-start-2 md:row-start-1 md:w-full'>
               {children}
             </div>
-            <div className='fixed bottom-0 h-16 w-screen shadow-xl md:static md:col-start-1 md:row-start-1 md:h-full md:w-full'>
+            <div className='h-16 w-screen shadow-xl md:static md:col-start-1 md:row-start-1 md:h-full md:w-full'>
               <NavBar />
             </div>
           </div>
@@ -45,3 +41,13 @@ const Layout = ({ children }: Props) => {
 };
 
 export default Layout;
+
+function Banner() {
+  return (
+    <Link href={'/'}>
+      <header className='flex h-14 w-full justify-center bg-blue-600 md:z-50 md:h-16'>
+        <Image src={Logo} width={300} height={56} alt='logo' />
+      </header>
+    </Link>
+  );
+}
